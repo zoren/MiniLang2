9180081a4fac6fd04b4d246a6b276ba30ae3018f
Søren Sjørup <soren.sjorup@deondigital.com>
2018-06-23 10:51:50 +0200

    added very basic language

---
+++ Lang.hs
@@ -0,0 +1,82 @@
+import Data.Maybe (fromMaybe)
+
+type PrimName = String
+type Constant = String -- atom
+type VariableName = String
+data Pattern
+  = PWildcard
+  | PConstant Constant
+  | PAlias Pattern VariableName
+  | PApply Pattern Pattern
+data Case = Case Pattern Expression
+data Cases
+  = CSingle Case
+  | CNext Case Cases
+data Expression
+  = EConstant Constant
+  | EVariable VariableName
+  | EPrim PrimName
+  | ELambda Cases
+  | EApply Expression Expression
+
+cases :: (Case -> a) -> (Case -> Cases -> a) -> Cases -> a
+cases single next css = case css of
+  CSingle cs -> single cs
+  CNext cs css' -> next cs css'
+
+-- interpreter
+  
+type Environment = [(VariableName, Value)]
+data Value
+  = VConstant Constant
+  | VApply Value Value
+  | VPrim PrimName
+  | VClosure Cases Environment
+
+match :: Pattern -> Environment -> Value -> Maybe Environment
+match pat enviroment value =
+  case pat of
+    PWildcard -> Just enviroment
+    PConstant pc ->
+      case value of
+        VConstant vc | pc == vc -> Just enviroment
+        _ -> Nothing
+    PAlias aliasedPattern alias -> do
+      env' <- match aliasedPattern enviroment value
+      return $ (alias, value) : (env' ++ enviroment)
+    PApply p1 p2 ->
+      case value of
+        VApply v1 v2 -> do
+          env1 <- match p1 enviroment v1
+          match p2 env1 v2
+        _ -> Nothing
+
+evalPrim :: PrimName -> Value -> Value
+evalPrim primName arg = case primName of
+  "atomEq" -> case arg of
+    VApply (VApply (VConstant "Pair") (VConstant v1)) (VConstant v2) ->
+      VConstant $ if v1 == v2 then "True" else "False"
+    _ -> error "atomEq got unexpected value"
+  _ -> error "unknown prim"
+
+interpret :: Expression -> Environment -> Value
+interpret expression enviroment =
+  case expression of
+    EConstant constant -> VConstant constant
+    EVariable var -> fromMaybe (error "variable not bound in environment") $ lookup var enviroment
+    EPrim prim -> VPrim prim
+    ELambda cases -> VClosure cases enviroment
+    EApply e1 e2 -> let
+      v1 = interpret e1 enviroment
+      v2 = interpret e2 enviroment
+      in case v1 of
+           VConstant {} -> VApply v1 v2
+           VApply {} -> VApply v1 v2
+           VPrim primName -> evalPrim primName v2
+           VClosure ccases cenviroment -> go ccases
+             where
+               go cs = let
+                 (Case pat exp, cont) = cases
+                   (\cs -> (cs, error "pattern match not exhaustive"))
+                   (\cs css' -> (cs, go css')) cs
+                 in maybe cont (interpret exp) $ match pat cenviroment v2
