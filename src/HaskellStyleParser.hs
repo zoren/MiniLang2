f8c2116b5978d44adc4fa1bbb2ab05ac50889eb2
Søren Sjørup <soren.sjorup@deondigital.com>
2018-09-15 13:02:49 +0200

    minor parser tweaks

--- src/HaskellStyleParser.hs
+++ src/HaskellStyleParser.hs
@@ -26,11 +26,11 @@ scn = lspace space1
 sc :: Parser ()
 sc = lspace $ void $ takeWhile1P Nothing $ \x -> x == ' ' || x == '\t'
 
-lexeme :: Parser a -> Parser a
-lexeme = L.lexeme sc
-
 trailingChars :: Parser Text
 trailingChars = takeWhileP Nothing $ \c -> isAlphaNum c || c == '_'
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme sc
 
 lowerId :: Parser LowerIdentifier
 lowerId = LowerIdentifier <$> lexeme (T.cons <$> lowerChar <*> trailingChars)
@@ -42,34 +42,35 @@ primId :: Parser PrimIndentifier
 primId = PrimIndentifier <$> lexeme (T.cons <$> char '$' *> trailingChars)
 
 sym :: Char -> Parser ()
-sym c = void $ lexeme $ char c
+sym = void . lexeme . char
+
+parens :: Parser a -> Parser a
+parens = between (sym '(') (sym ')')
+
+pcomb :: (a -> a -> a) -> Parser a -> Parser a
+pcomb f pelem = foldl1 f <$> some pelem
 
 ppattern :: Parser Pattern
-ppattern = foldl1 PApply <$> some patom
-  where
-    patom =
-      choice
-      [ PWildcard <$ sym '_'
-      , PConstant <$> upperId
-      , PParenthesis <$ sym '(' <*> ppattern <* sym ')'
-      , PVariable <$> lowerId
-      ]
+ppattern =
+  pcomb PApply $ choice
+  [ PWildcard <$ sym '_'
+  , PConstant <$> upperId
+  , PVariable <$> lowerId
+  , PParenthesis <$> parens ppattern
+  ]
 
 pexp :: Parser Expression
-pexp = foldl1 EApply <$> some eatom
-  where
-  eatom =
-    choice
-    [ EConstant <$> upperId
-    , EPrim <$> primId
-    , EVariable <$> lowerId
-    , ELambda <$ sym '\\' <*> sepBy1 lcase (sym '|')
-    , EParenthesis <$ sym '(' <*> pexp <* sym ')'
-    ]
-  lcase = Case <$> ppattern <* sym '.' <*> pexp
+pexp =
+  pcomb EApply $ choice
+  [ EConstant <$> upperId
+  , EVariable <$> lowerId
+  , EPrim <$> primId
+  , ELambda <$ sym '\\' <*> sepBy1 (Case <$> ppattern <* sym '.' <*> pexp) (sym '|')
+  , EParenthesis <$> parens pexp
+  ]
 
 pdecl :: Parser Declaration
 pdecl = ValueDeclaration <$> ppattern <* sym '=' <*> pexp
 
 pprog :: Parser Program
-pprog = many $ L.nonIndented scn pdecl <* scn
+pprog = many $ L.nonIndented scn pdecl
