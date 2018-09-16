8068e0f4e6e2e2b8c7365072863fb38549d76cc6
Søren Sjørup <soren.sjorup@deondigital.com>
2018-09-16 18:36:11 +0200

    added some prim functions

--- src/PrimEnv.hs
+++ src/PrimEnv.hs
@@ -3,10 +3,22 @@ module PrimEnv where
 
 import           Data.Function (fix)
 import qualified Data.Text as T
-import           Interpreter (apply, Value)
+import           Interpreter (apply, Value(..))
 import           Surface
 
-primMap :: Eq c => T.Text -> Value c Identifier -> Value c Identifier
+primMap :: T.Text -> Value Constant Identifier -> Value Constant Identifier
 primMap name = case name of
   "fix" -> fix . apply
+  "concat" ->
+    \arg ->
+      case arg of
+        (VConstant (CAtom "T") `VApply` (VConstant (CString s1)) `VApply` (VConstant (CString s2))) -> VConstant $ CString $ T.append s1 s2
+        _ -> error $ "concat unexpected arg: " ++ show arg
+  "strLen" ->
+    \(VConstant(CString s)) -> VConstant $ CInt $ T.length s
+  "index" ->
+    \arg ->
+      case arg of
+        (VConstant (CAtom "T") `VApply` (VConstant (CString s)) `VApply` (VConstant (CInt i))) -> VConstant $ CChar $ T.index s i
+        _ -> error $ "concat unexpected arg: " ++ show arg
   _ -> error "prim not known"
