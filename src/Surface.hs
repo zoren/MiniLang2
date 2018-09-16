ef3ac97a73a39bfd1d172de9894752562ed0aae6
Søren Sjørup <soren.sjorup@deondigital.com>
2018-09-16 18:51:08 +0200

    print constants more readable

--- src/Surface.hs
+++ src/Surface.hs
@@ -9,7 +9,14 @@ data Constant
   | CInt Int -- 0, 1, 2, 42
   | CString Text -- "", "a"
   | CChar Char -- 'a', `\n`
-  deriving (Eq, Show)
+  deriving (Eq)
+
+instance Show Constant where
+  show c = case c of
+    CAtom a -> unpack a
+    CInt i -> show i
+    CString s -> show s
+    CChar ch -> show ch
 
 data Pattern
   = PWildcard -- _
